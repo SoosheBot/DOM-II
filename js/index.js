b03ecfa04c66bc44ad887d1e509f61a60229f363
sooshe <sooshe@sooshebot.com>
2019-09-04 13:56:58 -0500

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 // Your code goes here
 
     // * [ x] `mouseover`
-	// * [ ] `keydown` no
+	// * [ X] `keydown`
 	// * [ x] `wheel`
 	// * [ ] `drag / drop`
 	// * [ ] `load`
 	// * [ ] `focus`
-	// * [ ] `resize`
+	// * [ x] `resize`
 	// * [ ] `scroll`
 	// * [ ] `select`
-    // * [ ] `dblclick`
+    // * [x ] `dblclick`
 
  //MOUSEOVER   
 const logoScale = document.querySelector('.logo-heading');
@@ -24,17 +24,28 @@ console.log(logoScale);
 //WHEEL
 const zoomNav = document.querySelector('.nav');
 zoomNav.addEventListener('wheel', event => {
-    zoomNav.transform = 'scale(2.1)';
-    zoomNav.transition = ' transform 0.5s'
+    zoomNav.style.backgroundColor = 'purple';
 });
 console.log(zoomNav);
 
-//KEYDOWN -- not working
-// const bigH2 = document.querySelector('.intro');
-//     bigH2.addEventListener('keydown', event => {
-//             bigH2.backgroundColor = 'purple';
-//           })
-//         })
-// console.log(bigH2);
-
 //RESIZE
+window.addEventListener('resize', event => {
+    const changeTitle = document.querySelector('.intro h2');
+    changeTitle.textContent='WELCOME TO THE FUNNEST BUS!'
+  });
+
+//KEYDOWN
+  window.addEventListener('keydown', event => {
+    const changePara = document.querySelector('.intro p');
+    changePara.textContent='OH SNAP! THIS IS THE FUNNEST BUS ON THE PLANET!'
+  });
+
+//DBLCLICK
+const gaudyH2  = document.querySelector('.content-section h2');
+gaudyH2.addEventListener('dblclick', function(event) {
+  console.log(`clicked`);
+  gaudyH2.style.color ='purple';
+  gaudyH2.style.transition = 'color 0.5s';
+   event.stopPropagation();
+});
+
