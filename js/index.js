be10a214472f38ad488eec5f21e11cb39264d183
sooshe <sooshe@sooshebot.com>
2019-09-04 16:27:52 -0500

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 // Your code goes here
 
     // * [ x] `mouseover`
-	// * [ X] `keydown`
-	// * [ x] `wheel`
-	// * [ ] `drag / drop`
-	// * [ ] `load`
-	// * [ ] `focus`
-	// * [ x] `resize`
-	// * [ ] `scroll`
-	// * [ ] `select`
     // * [x ] `dblclick`
+	// * [ X] `keydown`
+    // * [ x] `wheel`
+    // * [ x] `load`
+    // * [ x] `resize`
+    // * [x] `tweenmax` -- stretch
+	// * [ ] `drag / drop` no
+	// * [ x] `focus` 
+	// * [ ] `scroll` no
+	// * [ ] `select` no
+
+   
 
  //MOUSEOVER   
 const logoScale = document.querySelector('.logo-heading');
@@ -20,32 +23,69 @@ const logoScale = document.querySelector('.logo-heading');
 console.log(logoScale);
 
 
-
 //WHEEL
-const zoomNav = document.querySelector('.nav');
-zoomNav.addEventListener('wheel', event => {
-    zoomNav.style.backgroundColor = 'purple';
+const zoomFooter = document.querySelector('.footer');
+zoomFooter.addEventListener('wheel', event => {
+    zoomFooter.style.backgroundColor = 'purple';
+    zoomFooter.style.transition = 'color 0.1s';
+   event.stopPropagation();
 });
-console.log(zoomNav);
+console.log(zoomFooter);
+
 
 //RESIZE
 window.addEventListener('resize', event => {
     const changeTitle = document.querySelector('.intro h2');
-    changeTitle.textContent='WELCOME TO THE FUNNEST BUS!'
-  });
+    changeTitle.textContent = 'WELCOME TO THE FUNNEST BUS!'
+});
+
 
 //KEYDOWN
-  window.addEventListener('keydown', event => {
+window.addEventListener('keydown', event => {
     const changePara = document.querySelector('.intro p');
     changePara.textContent='OH SNAP! THIS IS THE FUNNEST BUS ON THE PLANET!'
   });
 
+
 //DBLCLICK
-const gaudyH2  = document.querySelector('.content-section h2');
-gaudyH2.addEventListener('dblclick', function(event) {
+const gaudyH2 = document.querySelector('.content-section h2');
+gaudyH2.addEventListener('dblclick', event => {
   console.log(`clicked`);
   gaudyH2.style.color ='purple';
   gaudyH2.style.transition = 'color 0.5s';
    event.stopPropagation();
+
+gaudyH2.addEventListener('dblclick', event => {
+     console.log(`clicked`);
+     gaudyH2.style.color ='teal';
+     gaudyH2.style.transition = 'color 0.5s';
+      event.stopPropagation();
+   });
 });
 
+
+
+//LOAD
+window.addEventListener('load', event => {
+    const changeMoreText = document.querySelector('.inverse-content h2');
+    changeMoreText.style.color ='orange';
+    changeMoreText.style.transition = 'color 3.0s';
+  });
+
+
+//SELECT
+
+
+
+//TWEENMAX
+ TweenMax.to(".destination h4", 0.5, {
+  rotationY: 180,
+  ease: Power1.easeInOut,
+}); 
+
+
+//FOCUS
+const gaudyNav = document.querySelector(".nav a");
+gaudyNav.addEventListener("focus", event =>{
+    gaudyNav.style.color = "red";
+});
